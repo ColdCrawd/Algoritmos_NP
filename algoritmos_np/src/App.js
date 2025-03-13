3c2739f13c050901cd830ac986647bab62506eb9
Elkyn <elkynmendoza@unitec.edu>
2025-03-12 20:55:14 -0600

    Feat: Added Main Layout

--- algoritmos_np/src/App.js
+++ algoritmos_np/src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { Layout, Menu, theme, Typography } from 'antd';
+import { Footer } from 'antd/es/layout/layout';
+const { Header, Content } = Layout;
+
 
 function App() {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Layout>
+      <Header
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <Typography.Title level={3} style={{ color: 'white', marginBottom:'25px', marginRight: '25px'}}> Analisis de Algoritmos </Typography.Title>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          items={[{ key: '1', label: 'Problema del Agente Viajero' }, { key: '2', label: 'nav 2' }]}
+          style={{
+            flex: 1,
+            minWidth: 0,
+          }}
+        />
+      </Header>
+      <Content
+        style={{
+          padding: '20px 48px',
+        }}
+      >
+        <div
+          style={{
+            background: colorBgContainer,
+            minHeight: '120vh',
+            padding: 24,
+            borderRadius: borderRadiusLG,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Content
+        </div>
+      </Content>
+      <Footer
+        style={{
+          textAlign: 'center',
+        }}
+      >
+        Ant Design ©{new Date().getFullYear()} Created by Ant UED
+      </Footer>
+    </Layout>
   );
 }
 
